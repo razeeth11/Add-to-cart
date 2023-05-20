f8a172448ab1572a48b9e453e1589ab33bc1271b
razeeth11 <razeeth011@gmail.com>
2023-05-20 09:51:14 +0530

    Products-Comp-

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react'
 import './App.css'
+import { products } from './data'
 
 function App() {
 
+  const [data,setData] = useState(products)
+
   return (
     <div className='App'>
+       {data.map((item,index)=> <ProductsComp props={item} key={index}/>)}
+    </div>
+  )
+}
 
+function ProductsComp({props}){
+  const {Price,MobileName,img,Brand,Offer} = props
+
+  return(
+    <div className="products">
+      
     </div>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,72 @@
+export const products = [
+  {
+    "MobileName": "Galaxy S10",
+    "Brand": "Samsung",
+    "img" : "./src/images/samsung.png",
+    "Price": "1099.99",
+    "Offer": "10%"
+  },
+  {
+    "MobileName": "iPhone 11",
+    "Brand": "Apple",
+    "img" : "./src/images/iphone.png",
+    "Price": "999.99",
+    "Offer": "5%"
+  },
+  {
+    "MobileName": "Moto G8 Plus",
+    "Brand": "Motorola",
+    "img" : "./src/images/moto.png",
+    "Price": "299.99",
+    "Offer": "10%"
+  },
+  {
+    "MobileName": "Realme 5 Pro",
+    "Brand": "Realme",
+    "img" : "./src/images/realme.png",
+    "Price": "249.99",
+    "Offer": "10%"
+  },
+  {
+    "MobileName": "Poco F1",
+    "Brand": "Xiaomi",
+    "img" : "./src/images/poco.png",
+    "Price": "399.99",
+    "Offer": "10%"
+  },
+  {
+    "MobileName": "ZenFone 6",
+    "Brand": "Asus",
+    "img" : "./src/images/zenfone.png",
+    "Price": "599.99",
+    "Offer": "10%"
+  },
+  {
+    "MobileName": "Nokia 7.2",
+    "Brand": "Nokia",
+    "img" : "./src/images/nokia.png",
+    "Price": "349.99",
+    "Offer": "15%"
+  },
+  {
+    "MobileName": "Vivo V17 Pro",
+    "Brand": "Vivo",
+    "img" : "./src/images/poco.png",
+    "Price": "499.99",
+    "Offer": "10%"
+  },
+  {
+    "MobileName": "Oppo Reno 2F",
+    "Brand": "Oppo",
+    "img" : "./src/images/opo.png",
+    "Price": "399.99",
+    "Offer": "10%"
+  },
+  {
+    "MobileName": "Redmi Note 8 Pro",
+    "Brand": "Xiaomi",
+    "img" : "./src/images/realme.png",
+    "Price": "399.99",
+    "Offer": "10%"
+  }
+]
